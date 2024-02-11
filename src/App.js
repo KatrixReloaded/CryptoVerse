be72c393d04db92818d7be95d406fc1e295bf3e6
kartikeyasureka <kartikeyasureka@gmail.com>
2024-02-11 17:48:11 +0530

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+        <div className='navbar'>
+            <Navbar />
+        </div>
+        <div className='main'>
+            <Layout>
+              <div className='routes'>
+                <Switch> /* Allows app to have multiple routes */
+                  <Route exact path = "/">
+                    <Homepage />
+                  </Route>
+                  <Route exact path = "/exchanges">
+                    <Exchanges />
+                  </Route>
+                  <Route exact path = "/cryptocurrencies">
+                    <Cryptocurrencies />
+                  </Route>
+                  <Route exact path = "/crypto/:coinId"> /* : means that the path is dynamic */
+                    <CryptoDetails />
+                  </Route>
+                  <Route exact path = "/news"> /* : means that the path is dynamic */
+                    <News />
+                  </Route>
+                </Switch>
+              </div>
+            </Layout>
+        </div>
+        <div className='footer'>
+
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
